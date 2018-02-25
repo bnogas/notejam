74495464bb2c5db84885810cd7913620a49f13be
Bartlomiej Nogas <bart.nogas@gmail.com>
2018-02-25 15:20:25 +0100

    Fix docker entrypoint

--- docker/get_credentials.py
+++ docker/get_credentials.py
@@ -7,6 +7,7 @@ url = "http://169.254.170.2" + os.environ['AWS_CONTAINER_CREDENTIALS_RELATIVE_UR
 resp = requests.get(url=url)
 data = json.loads(resp.text)
 bucket_name = data['RoleArn'].split('/')[-1]
+project_path = os.environ.get("PROJECT_PATH")
 
 client = boto3.client(
     's3',
@@ -15,5 +16,7 @@ client = boto3.client(
     aws_session_token=data['Token']
 )
 
-client.download_file(bucket_name, 'secret_env.sh', 'secret_env.sh')
-client.download_file(bucket_name, 'db_config.sh', 'db_config.sh')
+client.download_file(
+    bucket_name, 'secret_env.sh', '{}/secret_env.sh'.format(project_path))
+client.download_file(
+    bucket_name, 'db_config.sh', '{}/db_config.sh'.format(project_path))
